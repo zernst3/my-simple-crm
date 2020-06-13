e95d755a2910de655ecaa9b25ba685963aa1ef0a
Zachary Ernst <zernst3@live.com>
2020-06-13 14:17:06 -0400

    Changed app.listen to port

--- app.js
+++ app.js
@@ -122,6 +122,6 @@ app.get("*", isLoggedIn, function(req, res){
 
 // =======================Server
 
-app.listen(3000, function() { 
-  console.log('Server listening on port 3000'); 
+app.listen(port, function() { 
+  console.log('Server has started'); 
 });
